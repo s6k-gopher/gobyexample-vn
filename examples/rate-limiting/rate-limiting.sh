bf86cbe3a89cfa8f401ee77f237702c168472107
hqphuoc129 <hqphuoc129@gmail.com>
2023-04-15 02:20:51 +0700

    transalte mutexes, rate-limiting, tickers, waitgroups, worker pools

---
+++ examples/rate-limiting/rate-limiting.sh
@@ -0,0 +1,17 @@
+# Khi thực thi chương trình, chúng ta thấy rằng các yêu cầu 
+# của batch đầu tiên được xử lý một lần mỗi 200 mili giây như mong muốn.
+$ go run rate-limiting.go
+request 1 2012-10-19 00:38:18.687438 +0000 UTC
+request 2 2012-10-19 00:38:18.887471 +0000 UTC
+request 3 2012-10-19 00:38:19.087238 +0000 UTC
+request 4 2012-10-19 00:38:19.287338 +0000 UTC
+request 5 2012-10-19 00:38:19.487331 +0000 UTC
+
+# Đối với batch request thứ hai, chúng ta xử lý 3 request đầu tiên
+# ngay lập tức nhờ vào khả năng tăng tốc của rate limiting,
+# sau đó xử lý 2 request còn lại với khoảng thời gian trễ ~200ms mỗi request.
+request 1 2012-10-19 00:38:20.487578 +0000 UTC
+request 2 2012-10-19 00:38:20.487645 +0000 UTC
+request 3 2012-10-19 00:38:20.487676 +0000 UTC
+request 4 2012-10-19 00:38:20.687483 +0000 UTC
+request 5 2012-10-19 00:38:20.887542 +0000 UTC
---
+++ examples/worker-pools/worker-pools.sh
@@ -0,0 +1,17 @@
+# Chương trình được thực thi sẽ hiển thị 5 tác vụ được thực thi bởi 
+# các worker khác nhau. Chương trình chỉ mất khoảng 2 giây để thực thi, 
+# mặc dù nó thực hiện các tác vụ trong khoảng tổng cộng là 5 giây, 
+# vì có 3 worker chạy song song.
+$ time go run worker-pools.go 
+worker 1 started  job 1
+worker 2 started  job 2
+worker 3 started  job 3
+worker 1 finished job 1
+worker 1 started  job 4
+worker 2 finished job 2
+worker 2 started  job 5
+worker 3 finished job 3
+worker 1 finished job 4
+worker 2 finished job 5
+
+real	0m2.358s
