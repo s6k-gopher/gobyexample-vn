98ca83ad66157052d4621e733b6e0b1a23157fde
hqphuoc129 <hqphuoc129@gmail.com>
2023-04-15 02:22:46 +0700

    Revert "transalte mutexes, rate-limiting, tickers, waitgroups, worker pools"
    This reverts commit bf86cbe3a89cfa8f401ee77f237702c168472107.

--- examples/tickers/tickers.sh
+++
@@ -1,7 +0,0 @@
-# Khi chúng ta chạy chương trình này thì ticker sẽ lặp lại 3 lần
-# trước khi chúng ta dừng nó.
-$ go run tickers.go
-Tick at 2012-09-23 11:29:56.487625 -0700 PDT
-Tick at 2012-09-23 11:29:56.988063 -0700 PDT
-Tick at 2012-09-23 11:29:57.488076 -0700 PDT
-Ticker stopped
