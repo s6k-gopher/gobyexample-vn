8330c2dc92053b5eab777436f83d60a91c2eaca3
Ha Quang Phuoc <107381046+hqphuoc@users.noreply.github.com>
2023-04-26 09:57:16 +0700

    Update signals.sh

--- examples/signals/signals.sh
+++ examples/signals/signals.sh
@@ -1,7 +1,9 @@
 # Khi ta chạy chương trình này, nó sẽ chờ 
-# một tín hiệu. Bằng cách gõ `ctrl-C` (terminal sẽ hiển thị là `^C`)
+# một tín hiệu. Bằng cách gõ `ctrl-C` 
+# (terminal sẽ hiển thị là `^C`)
 # ta có thể gửi một tín hiệu `SIGINT`,
-# khiến cho chương trình in ra `interrupt` và sau đó thoát.
+# khiến cho chương trình in ra `interrupt` 
+# và sau đó thoát.
 $ go run signals.go
 awaiting signal
 ^C
